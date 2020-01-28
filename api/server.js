6b7919cb6c05d302b782bc225efa39f9eb223393
Juan Aleman <juan.j.aleman@gmail.com>
2020-01-27 19:44:48 -0600

    projects CRUD done

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
+const projectsRouter = require("../endpoints/projectsRouter");
+const actionsRouter = require("../endpoints/actionsRouter");
+
 
 const server = express();
 
 
 server.use(express.json());
+server.use("/projects", projectsRouter);
+server.use("/actions", actionsRouter);
 
 
 
